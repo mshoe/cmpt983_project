8cbc89450f2576e63de4d1535313b3704a655845
Tristan Engst <tristanengst@gmail.com>
2023-11-30 21:24:11 -0800

    MV stuff

---
+++ algorithms/eps_greedy_mv.py
@@ -0,0 +1,120 @@
+import numpy as np
+import random
+from util.bandit_util import BanditMachine
+from util.bandit_algorithm import BanditAlgorithm
+import copy
+import abc
+
+class eps_greedy(BanditAlgorithm):
+    def __init__(self, _bandit_machine: BanditMachine, initial_eps, rho=1, cutoff=0):
+        self._initial_eps = initial_eps
+        self._curr_eps = initial_eps
+        self._bandit_machine = copy.deepcopy(_bandit_machine)
+        self._rho = rho
+        self._cutoff = cutoff
+        return
+    
+    @abc.abstractmethod
+    def _get_eps(self, t):
+        # called once a round
+        return self._initial_eps
+    
+    def run_alg(self, T: int):
+        bandit_machine = copy.deepcopy(self._bandit_machine)
+
+        init_num_arms = bandit_machine.num_arms
+        
+        sample_means = np.zeros(shape=(init_num_arms,), dtype=float)
+        sample_vars = np.zeros(shape=(init_num_arms,), dtype=float)
+        sample_goodness = np.zeros(shape=(init_num_arms,), dtype=float)
+        sample_pulls = np.zeros(shape=(init_num_arms,), dtype=int)
+        
+        total_reward_per_round = np.zeros(shape=(T,), dtype=float)
+        total_reward = 0.0
+
+        best_mean, best_arm = bandit_machine.get_max_mean()
+        
+        total_best_exp_reward_per_round = np.zeros(shape=(T,), dtype=float)
+        total_best_exp_reward = 0.0
+
+        total_exp_reward_per_round = np.zeros(shape=(T,), dtype=float)
+        total_exp_reward = 0.0
+
+        # select arms until max rounds
+        num_arms = init_num_arms
+        all_arms_pulled_once = False
+        
+        self._curr_eps = self._initial_eps
+
+        count_below_cutoff = 0
+
+        for t in range(0, T):
+
+            # check if arms are acquired this round
+            arms_added = bandit_machine.acquire_arms()
+            if arms_added:
+                num_arms = bandit_machine.num_arms
+                sample_means.resize(num_arms)
+                sample_pulls.resize(num_arms)
+                sample_vars.resize(num_arms)
+                sample_goodness.resize(num_arms)
+                best_mean, best_arm = bandit_machine.get_max_mean()
+                all_arms_pulled_once = False
+                self._curr_eps = self._initial_eps
+
+            # exploit or explore
+            val = random.random()
+
+            eps = self._get_eps(t)
+
+            if val <= 1.0 - eps:
+                a = sample_goodness.argmax()
+            else:
+                # a = np.argmin(sample_pulls)
+                a = random.randint(0, num_arms-1)
+
+            # mandatory exploration of unpulled arms
+            if not all_arms_pulled_once:
+                for arm_index in range(num_arms):
+                    if sample_pulls[arm_index] == 0:
+                        a = arm_index
+                        break
+                if arm_index >= num_arms - 1:
+                    all_arms_pulled_once = True
+
+            # update sample means and number of pulls
+            sample_reward = bandit_machine.arms[a].pull()
+            sample_means[a] = (sample_pulls[a] * sample_means[a] + sample_reward) / (sample_pulls[a] + 1.0)
+            sample_var[a] = (sample_var[a] * sample_pulls[a] + (sample_reward - sample_means[a]) ** 2) / (sample_pulls[a] + 1.0)
+            sample_goodness[a] = sample_means[a] - self._rho * sample_var[a]
+            sample_pulls[a] += 1
+
+            # update reward and best reward for plots
+            total_reward += sample_reward
+            total_reward_per_round[t] = total_reward
+
+            total_exp_reward += bandit_machine.arms[a].expected_reward()
+            total_exp_reward_per_round[t] = total_exp_reward
+
+            total_best_exp_reward += best_mean
+            total_best_exp_reward_per_round[t] = total_best_exp_reward
+
+            count_below_cutoff += 1 if sample_reward < self._cutoff else 0
+
+        print(bandit_machine.arms[sample_goodness.argmax()])
+        return total_reward_per_round, total_exp_reward_per_round, total_best_exp_reward_per_round
+
+
+class decay_eps_greedy(eps_greedy):
+    def __init__(self, _bandit_machine: BanditMachine, initial_eps, decay_factor, rho=1):
+        super().__init__(_bandit_machine, initial_eps)
+        self._curr_eps = initial_eps
+        self._decay_factor = decay_factor
+        self._rho = rho
+        return
+    
+    def _get_eps(self, t):
+        eps = self._curr_eps
+        self._curr_eps *= self._decay_factor
+        return eps
+    
---
+++ algorithms/ucb_mv.py
@@ -0,0 +1,110 @@
+import numpy as np
+import random
+from util.bandit_util import BanditMachine
+from util.bandit_algorithm import BanditAlgorithm
+import copy
+import abc
+class ucb(BanditAlgorithm):
+    def __init__(self, _bandit_machine: BanditMachine, rho=1, cutoff=0):
+        self._bandit_machine = copy.deepcopy(_bandit_machine)
+        self._rho = rho
+        self._cutoff = cutoff
+        return
+    
+    @abc.abstractmethod
+    def _get_std_coeff(self, t):
+        return
+
+    def run_alg(self, T: int):
+        bandit_machine = copy.deepcopy(self._bandit_machine)
+        K = bandit_machine.num_arms
+
+        U = np.ones(K) * np.inf
+        mu = np.zeros(K)
+        var = np.zeros(K)
+        goodness = np.zeros(K)
+        N = np.zeros(K)
+        total_reward = 0.0
+        total_reward_per_round = np.zeros(T)
+
+        total_exp_reward = 0.0
+        total_exp_reward_per_round = np.zeros(T)
+
+        total_best_reward = 0.0
+        total_best_exp_reward_per_round = np.zeros(T)
+
+        best_mean, best_arm = bandit_machine.get_max_mean()
+
+        count_below_cutoff = 0
+
+        for t in range(0, T):
+
+            # check if arms are acquired this round
+            arms_added = bandit_machine.acquire_arms()
+            if arms_added:
+                old_K = K
+                K = bandit_machine.num_arms
+                U.resize(K)
+                for i in range(old_K, K):
+                    U[i] = np.inf
+                mu.resize(K)
+                var.resize(K)
+                N.resize(K)
+                goodness.resize(K)
+                best_mean, best_arm = bandit_machine.get_max_mean()
+
+            a = np.argmax(U) # in case of ties, argmax returns the index of first occurance
+            reward = bandit_machine.arms[a].pull()
+            
+            mu[a] = (mu[a] * N[a] + reward) / (N[a] + 1.0)
+            var[a] = (var[a] * N[a] + (reward - mu[a]) ** 2) / (N[a] + 1.0)
+            goodness[a] = mu[a] - self._rho * var[a]
+            N[a] += 1.0
+            
+            std_coeff = self._get_std_coeff(t)
+            U[a] = goodness[a] + std_coeff / np.sqrt(N[a])
+
+            total_reward += reward
+            total_reward_per_round[t] = total_reward
+
+            total_exp_reward += bandit_machine.arms[a].expected_reward()
+            total_exp_reward_per_round[t] = total_exp_reward
+
+            total_best_reward += best_mean
+            total_best_exp_reward_per_round[t] = total_best_reward
+
+            count_below_cutoff += 1 if reward < self._cutoff else 0
+
+        print(bandit_machine.arms[np.argmax(U)])
+        return total_reward_per_round, total_exp_reward_per_round, total_best_exp_reward_per_round, count_below_cutoff
+    
+class ucb_basic(ucb):
+    def __init__(self, _bandit_machine: BanditMachine, T: int):
+        super().__init__(_bandit_machine)
+        delta = 1.0 / T**2
+        self._std_coeff = np.sqrt(2.0 * np.log(1.0/delta))
+        return
+    
+    def _get_std_coeff(self, t):
+        return self._std_coeff
+    
+class auer(ucb):
+    def __init__(self, _bandit_machine: BanditMachine):
+        super().__init__(_bandit_machine)
+        return
+    
+    def _get_std_coeff(self, t):
+        # + 1.0 since t starts at 0
+        t = t + 1.0
+        return np.sqrt(8.0 * np.log(t))
+    
+class ucb_AO(ucb):
+    def __init__(self, _bandit_machine: BanditMachine):
+        super().__init__(_bandit_machine)
+        return
+    
+    def _get_std_coeff(self, t):
+        # + 1.0 since t starts at 0
+        t = t + 1.0
+        f = 1.0 + t * (np.log(t))**2
+        return np.sqrt(2.0 * np.log(f))
